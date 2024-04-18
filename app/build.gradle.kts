b2fc48d607b55d9140ad736904fdea87b121c52e
mhelder <matthias.helder@justpinch.com>
2024-04-18 10:29:21 +0200

    Full repro project

---
+++ app/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.okhttpexosample"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.okhttpexosample"
+        minSdk = 23
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
+    val media3Version = "1.3.1"
+    implementation("androidx.media3:media3-ui:$media3Version")
+    implementation("androidx.media3:media3-common:$media3Version")
+    implementation("androidx.media3:media3-exoplayer:$media3Version")
+    implementation("androidx.media3:media3-datasource-okhttp:$media3Version")
+}
---
+++ app/src/main/java/com/example/okhttpexosample/MainActivity.kt
@@ -0,0 +1,104 @@
+package com.example.okhttpexosample
+
+import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.media3.common.MediaItem
+import androidx.media3.common.PlaybackParameters
+import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.datasource.DataSource
+import androidx.media3.datasource.DefaultHttpDataSource
+import androidx.media3.datasource.okhttp.OkHttpDataSource
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.DefaultMediaSourceFactory
+import com.example.okhttpexosample.databinding.ActivityMainBinding
+import okhttp3.OkHttpClient
+import okhttp3.Protocol
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    @UnstableApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding.playerControlView) {
+            // show controller and keep showing it
+            showController()
+            controllerShowTimeoutMs = -1
+            controllerHideOnTouch = false
+        }
+
+        // default http stack
+        binding.defaultHttpButton.setOnClickListener {
+            releasePlayer()
+
+            val httpDataSource = DefaultHttpDataSource.Factory()
+            binding.playerControlView.player = buildPlayer(httpDataSource)
+                .also(Player::prepareAndPlayItem)
+        }
+
+        // default okhttp (http2 enabled)
+        binding.okhttpHttp2Button.setOnClickListener {
+            releasePlayer()
+
+            val okHttpClient = OkHttpClient.Builder()
+                .build()
+            val httpDataSource = OkHttpDataSource.Factory(okHttpClient)
+            binding.playerControlView.player = buildPlayer(httpDataSource)
+                .also(Player::prepareAndPlayItem)
+        }
+
+        // okhttp forced to http1.1
+        binding.okhttpHttp1Button.setOnClickListener {
+            releasePlayer()
+
+            val okHttpClient = OkHttpClient.Builder()
+                .protocols(listOf(Protocol.HTTP_1_1))
+                .build()
+            val httpDataSource = OkHttpDataSource.Factory(okHttpClient)
+            binding.playerControlView.player = buildPlayer(httpDataSource)
+                .also(Player::prepareAndPlayItem)
+        }
+    }
+
+    private fun releasePlayer() {
+        with(binding.playerControlView) {
+            player?.release()
+            player = null
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        releasePlayer()
+    }
+}
+
+private fun Context.buildPlayer(
+    httpDataSource: DataSource.Factory,
+): Player {
+    val mediaSourceFactory = DefaultMediaSourceFactory(this)
+        .setDataSourceFactory(httpDataSource)
+
+    return ExoPlayer.Builder(applicationContext)
+        .setMediaSourceFactory(mediaSourceFactory)
+        .build()
+        .apply {
+            // speed up playback by an arbitrary high value
+            playbackParameters = PlaybackParameters(128f, 1f)
+        }
+}
+
+private fun Player.prepareAndPlayItem() {
+    setMediaItem(MediaItem.fromUri(mediaUrl))
+    prepare()
+    play()
+}
+
+private const val mediaUrl =
+    "https://25523.mc.tritondigital.com/OMNY_DSVANDAAGAPP_PODCAST_P/media-session/048a28d3-b74f-4782-ab7f-d07291f80ce1/d/clips/fdd7ab40-270d-4a1e-a257-acd200da1324/2e850afd-b63e-4791-ae3f-b0380101da98/47eba102-a0f8-4277-b44e-b0d1012b5f6f/audio/direct/t1701998186/Gaza_(2_2)_Ondanks_vredespogingen_dreven_Isra_l_en_Palestina_verder_uit_elkaar.mp3?t=1701998186&in_playlist=a4cdb64d-2881-4330-be5d-b0380101dbc7&utm_source=Podcast"
